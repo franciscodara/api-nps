f7840e8a4fba4d30f81d18a8fe7f1e0ced44054e
franciscodara <franciscodara@gmail.com>
2021-05-05 21:42:20 -0300

    typeorm migration-run

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * Métodos:
- * GET = busca
- * POST = salvar
- * PUT = alterar
- * DELETE = deletar
- * PATCH = altetação expecífica
- */
+app.use(express.json());
+app.use( router );
 
-//GET Method (app.metodo("rota ou recurdo", (request, response)))
-
-app.get("/",(request, response) => {
-    //return response.send("Hello world - NLW!");
-    return response.json({message:"Hello world - NLW!"});
-})
-
-app.listen(3333, () => console.log ("Server is running!!!")); 
-
-// POST Method (app.metodo("rota ou recurdo", (request, response)))
-
-app.post("/", (request, response) => {
-    //Após dados recebidos...
-    return response.json({message:"Dados recebidos com sucesso!"});
-})
+app.listen(3333, () => console.log ("Server is running!!!"));
