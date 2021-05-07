ebbba773da95a959d4790ab84872b9d76082b07b
franciscodara <franciscodara@gmail.com>
2021-05-07 15:56:49 -0300

    testes

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { app } from "../app";
+import createConnection from "../database";
+
+describe("Surveys", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new survey.", async () => {
+        const response = await request(app).post("/surveys")
+            .send({
+                title: "title example",
+                description: "Description example.",
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+    });;
+
+    it("Should be able to get all survey", async () => {
+        await request(app).post("/surveys")
+            .send({
+                title: "title example 002",
+                description: "Description example 002.",
+            });
+
+            const response = await request(app).get("/surveys");
+            expect(response.body.length).toBe(2);
+    })
+});
