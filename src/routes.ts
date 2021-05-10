41ec9cd5b475a7209161e974c7e019d88c21331f
franciscodara <franciscodara@gmail.com>
2021-05-10 19:23:54 -0300

    sendMailController

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { SendMailController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserControler";
 
@@ -6,10 +7,14 @@ const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
+ 
+// Quartra aula em 28min 54 seg
