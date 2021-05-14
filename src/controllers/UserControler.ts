b75b5d9fe8a536dc63798d4bcb59846954c6ad02
franciscodara <franciscodara@gmail.com>
2021-05-14 16:50:31 -0300

    Refatoraçao e calculo NPS

--- src/controllers/UserControler.ts
+++ src/controllers/UserControler.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("O nome é obrigatório!"),
+            email: yup.string().email().required("E-mail incorreto")
+        })
+
+        try {
+            await schema.validate(request.body, {abortEarly: false})
+        } catch (err) {
+            throw new AppError(err)
+        };
 
         const usersRepository = getCustomRepository(UsersRepository);
 
@@ -12,10 +25,8 @@ class UserController {
             email
         });
         if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!",
-            });
-        }
+            throw new AppError("User already exists!")
+        };
 
         const user = usersRepository.create({
             name,
